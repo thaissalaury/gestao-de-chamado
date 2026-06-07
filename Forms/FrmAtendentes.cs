b418b2af6ab2cacfade969318ae89c04ddf0820f
thaissalaury <thaissa@example.com>
2026-06-07 00:08:08 -0300

    feat: telas de cadastro de clientes, atendentes e chamados

---
+++ Forms/FrmAtendentes.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Forms;
+using GestaoChamados.Models;
+using GestaoChamados.Services;
+
+namespace GestaoChamados
+{
+    public partial class FrmAtendentes : Form
+    {
+        private AtendenteService atendenteService = new AtendenteService();
+
+        public FrmAtendentes()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            this.Text = "Cadastro de Atendentes";
+            this.Size = new Size(600, 500);
+            this.StartPosition = FormStartPosition.CenterParent;
+        }
+    }
+}
---
+++ Forms/FrmClientes.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Forms;
+using GestaoChamados.Models;
+using GestaoChamados.Services;
+
+namespace GestaoChamados
+{
+    public partial class FrmClientes : Form
+    {
+        private ClienteService clienteService = new ClienteService();
+
+        public FrmClientes()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            this.Text = "Cadastro de Clientes";
+            this.Size = new Size(600, 500);
+            this.StartPosition = FormStartPosition.CenterParent;
+        }
+    }
+}
