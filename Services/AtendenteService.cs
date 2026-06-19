fcf2f4afff3b103819a3a264d06d3651699c0fc6
thaissalaury <laurythaissa@gmail.com>
2026-06-19 00:34:17 -0300

    feat: validar exclusao atendente

--- Services/AtendenteService.cs
+++ Services/AtendenteService.cs
@@ -10,6 +10,7 @@ namespace GestaoChamados.Services
     public class AtendenteService
     {
         private readonly AtendenteRepository _atendenteRepository = new AtendenteRepository();
+        private readonly ChamadoRepository _chamadoRepository = new ChamadoRepository();
 
         private void GarantirPermissaoDeEscrita()
         {
@@ -36,16 +37,21 @@ namespace GestaoChamados.Services
             return _atendenteRepository.Listar();
         }
 
+        public Atendente? BuscarPorId(int id)
+        {
+            return _atendenteRepository.BuscarPorId(id);
+        }
+
         public void Excluir(int id)
         {
             GarantirPermissaoDeEscrita();
+
+            if (_chamadoRepository.ContarPorAtendente(id) > 0)
+            {
+                throw new InvalidOperationException("Não é possível excluir este atendente pois existem chamados vinculados a ele.");
+            }
+
             _atendenteRepository.Excluir(id);
-        }
-
-        public Atendente? BuscarPorId(int id)
-
-        {
-            return _atendenteRepository.BuscarPorId(id);
         }
     }
 }
